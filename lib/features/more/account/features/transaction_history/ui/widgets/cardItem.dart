e205e4e02d7380d44ca7f444cd3f823c5fa3cf10
Biju Mondal <biju.mondal@cbnits.com>
2022-04-03 20:39:10 +0530

    folow fanex screen redesigned

--- lib/features/more/account/features/transaction_history/ui/widgets/cardItem.dart
+++ lib/features/more/account/features/transaction_history/ui/widgets/cardItem.dart
@@ -1,4 +1,5 @@
 import 'package:fanex_flutter/common/common.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class TransactionHistoryCardItem extends StatelessWidget {
@@ -8,7 +9,11 @@ class TransactionHistoryCardItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(AppSizes.dimen8),
+      padding: const EdgeInsets.only(
+          left: AppSizes.dimen8,
+          top: AppSizes.dimen12,
+          bottom: AppSizes.dimen12,
+          right: 0),
       margin: const EdgeInsets.symmetric(
           horizontal: AppSizes.dimen12, vertical: AppSizes.dimen8),
       decoration: BoxDecoration(
@@ -22,11 +27,36 @@ class TransactionHistoryCardItem extends StatelessWidget {
             )
           ]),
       child: Row(
-        children: const [
-          Text('23-04-2022'),
-          Expanded(child: Center(child: Text('Winnings'))),
-          Text('+c 400'),
-          Icon(Icons.more_vert)
+        children: [
+          const Text('23-04-2022',
+              style: TextStyle(
+                  fontSize: AppSizes.bodyText2,
+                  fontWeight: FontWeight.w400,
+                  color: AppColors.darkGrey)),
+          const Expanded(
+              child: Center(
+                  child: Text('Winnings',
+                      style: TextStyle(
+                          fontSize: AppSizes.bodyText2,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.darkGrey)))),
+          const Text('+c 400',
+              style: TextStyle(
+                  fontSize: AppSizes.bodyText2,
+                  fontWeight: FontWeight.w400,
+                  color: AppColors.orange)),
+          InkWell(
+            onTap: () {
+              if (kDebugMode) {
+                print('info button pressed');
+              }
+            },
+            child: const Icon(
+              Icons.more_vert,
+              color: AppColors.green,
+              size: 20,
+            ),
+          )
         ],
       ),
     );
