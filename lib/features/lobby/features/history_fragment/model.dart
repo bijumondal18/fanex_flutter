39fcb4144e331b154a807d448391359720e2e37b
sitabra <sitabra.das@cbnits.com>
2022-05-09 17:23:56 +0530

    custom container in Ranking page and History fragment updated with list but scroll not solved

---
+++ lib/features/lobby/features/history_fragment/model.dart
@@ -0,0 +1,64 @@
+List<String> teamFirst = [
+  "CSK",
+  "KKR",
+  "SRH",
+  "MI",
+  "RCB",
+  "DC",
+  "RR",
+  "KXIP",
+  "LSG",
+  "GT"
+];
+
+List<String> teamSecond = [
+  "GT",
+  "LSG",
+  "KXIP",
+  "RR",
+  "DC",
+  "RCB",
+  "MI",
+  "SRH",
+  "KKR",
+  "CSK"
+];
+
+List<String> entryFee = [
+  "100",
+  "50",
+  "300",
+  "500",
+  "70",
+  "90",
+  "150",
+  "30",
+  "80",
+  "50"
+];
+
+List<double> progressValue = [
+  0.27,
+  0.90,
+  0.70,
+  0.26,
+  0.90,
+  0.75,
+  0.85,
+  0.30,
+  0.55,
+  0.85,
+];
+
+List<String> wonAmount = [
+  "123",
+  "147",
+  "258",
+  "369",
+  "456",
+  "852",
+  "741",
+  "320",
+  "951",
+  "357"
+];
